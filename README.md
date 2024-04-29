6e8ba59d45bb1064851e49146d84d30a2eb083ff
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-04-29 12:46:26 +0500

    first commit, build up project

---
+++ README.md
@@ -0,0 +1,93 @@
+## Welcome to
+# Master Exam System (MAS)
+
+
+### About the Project
+
+soon...
+
+### About the BackEnd
+
+soon...
+
+### About the FrontEnd
+You can view project in github with link: 
+
+
+***
+
+## Tech
+
+* [Django](https://www.djangoproject.com/) - is a high-level `Python Web framework`
+* [Django REST framework](https://www.django-rest-framework.org/) - `Django REST Framework` is a powerful and flexible toolkit for building Web `APIs`
+* [PostgreSQL](https://www.postgresql.org/) - open source object-relational database system
+
+And many other libraries.
+
+Dillinger requires [Python](https://www.python.org) `v3.10` or `v3.11`.
+
+```shell
+$ git clone git@github.com:Abduraxmonnn/MasterExamSystem.git
+$ cd master_exam_system
+```
+
+***
+
+## Setting project
+
+* `Linux`
+```shell
+$ virtualenv -p /usr/bin/python3 .venv
+$ source .venv/bin/activate
+$ pip install -r requirements.txt
+```
+
+* `Windows`
+```shell
+$ python -m venv ./venv
+$ venv\Scripts\activate
+$ pip install -r requirements.txt
+```
+
+* `MacBook`
+```shell
+$ python3 -m venv venv
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+```
+
+***
+
+## Development
+### Configure `PostgreSQL`
+Create clear database named `master_sys_db`.
+
+Create `master_sys_user` db user with password `master_1234` and grand privileges to him.
+
+If you want to create a database with a different name, user and password, you can change the initial configuration to your own configuration.
+```shell
+$ sudo -u postgres psql
+postgres=# ...
+CREATE DATABASE master_sys_db;
+CREATE USER master_sys_user WITH PASSWORD 'master_12345';
+ALTER ROLE master_sys__user SET client_encoding TO 'utf8';
+ALTER ROLE master_sys_user SET default_transaction_isolation TO 'read committed';
+ALTER ROLE master_sys_user SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE master_sys_db TO master_sys_user;
+\q
+```
+Migrate to database and run project.
+```shell
+$ python manage.py makemigrations
+$ python manage.py migrate
+$ python manage.py runserver
+```
+`Output`
+```shell
+System check identified no issues (0 silenced).
+Month date, year - hh:mm:ss
+Django version 4.1.7, using settings 'config.settings'
+Starting development server at http://127.0.0.1:8000/
+Quit the server with CONTROL-C.
+```
+Open http://127.0.0.1:8000 in your browser for see result.
---
+++ config/settings.py
@@ -0,0 +1,186 @@
+import sys
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from corsheaders.defaults import default_headers
+
+load_dotenv()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Append module dir
+sys.path.append(os.path.join(BASE_DIR, 'apps'))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get('DEBUG')
+
+ALLOWED_HOSTS = ['*']
+
+INTERNAL_IPS = [
+        '127.0.0.1'
+    ]
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    
+    # Libraries
+    'debug_toolbar',
+    'django_filters',
+    'rest_framework',
+    'drf_yasg',
+    'corsheaders',
+
+    # Apps
+    ]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'debug_toolbar.middleware.DebugToolbarMiddleware'
+]
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DB_SQLITE = "sqlite"
+DB_POSTGRESQL = "postgresql"
+DB_POSTGRESQL_DOCKER = "postgresql_docker"
+
+DATABASES_ALL = {
+    DB_SQLITE: {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+    DB_POSTGRESQL: {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_NAME', 'test_db'),
+        'USER': os.environ.get('POSTGRES_USER', 'test_user'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'test_12345'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+    },
+    DB_POSTGRESQL_DOCKER: {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('POSTGRES_NAME_DOCKER', 'test_db_prod'),
+        'USER': os.environ.get('POSTGRES_USER_DOCKER', 'test_user_prod'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD_DOCKER', 'test_12345_prod'),
+        'HOST': os.environ.get('POSTGRES_HOST_DOCKER', 'db'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+    }
+}
+
+DATABASES = {"default": DATABASES_ALL[os.environ.get("BACKEND_DB")]}
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ],
+
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 50,
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_HEADERS = default_headers + (
+    'cache-control',
+)
+
