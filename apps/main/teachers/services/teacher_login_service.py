cca459b83f9b44a37d0dd7378bdb8c2a2a781496
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-06-01 14:59:44 +0500

    Fixed bug. When TEacher login subjects duplicated

--- apps/main/teachers/services/teacher_login_service.py
+++ apps/main/teachers/services/teacher_login_service.py
@@ -28,13 +28,11 @@ def teacher_login_data_checker(teacher_obj):
         generate_token = CustomToken().generate_key()
         token = CustomToken.objects.create(teacher=teacher_obj, is_student=False, key=generate_token)
 
-    teacher_subject_code = []
-    for item in get_subjects:
-        tmp_res = {
-            'name': item.subject.full_name,
-            'code': item.subject.code
-        }
-        teacher_subject_code.append(tmp_res)
+    unique_subjects = get_subjects.distinct('subject__id')
+    teacher_subject_code = [
+        {'name': item.subject.full_name, 'code': item.subject.code}
+        for item in unique_subjects
+    ]
 
     data = {
         'token': token.key,
