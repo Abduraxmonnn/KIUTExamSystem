08321b201f7977cef2d8963fa8229134b661f1ef
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-06-03 10:34:36 +0500

    Changed logic of set score to student by teachers

--- apps/main/teachers/services/teacher_set_score_to_student_service.py
+++ apps/main/teachers/services/teacher_set_score_to_student_service.py
@@ -5,7 +5,6 @@ from rest_framework.response import Response
 # Project
 from apps.main.answers.models import Answer
 from apps.main.students.models import Student
-from apps.main.subjects.models import Subject
 from apps.main.teachers_subjects.models import TeacherSubject
 from apps.services.get_user_by_token_service import get_user_by_token
 
@@ -33,18 +32,16 @@ def set_score_to_student(
     try:
         teacher_obj = get_user_by_token(request)
         answer_obj = Answer.objects.filter(subject__code=subject_code, student__student_id=student_id, stage=stage)
-        student_group_obj = Student.objects.get(student_id=student_id)
-        student_group_letter = student_group_obj.group.code[-1]
-        print('--> ', answer_obj.last().subject)
-        teacher_subject_obj = TeacherSubject.objects.get(teacher=teacher_obj, subject=answer_obj.last().subject,
-                                                         language=groups_picker[student_group_letter])
+        student_obj = Student.objects.get(student_id=student_id)
+        teacher_subject_obj = TeacherSubject.objects.filter(teacher=teacher_obj, subject=answer_obj.last().subject)
+        teacher_subject_list = [i.group.code for i in teacher_subject_obj]
     except Exception as ex:
         return Response({
             'status': 'error',
             'message': f'Raise error while getting Objects, {ex}'
         }, status=status.HTTP_400_BAD_REQUEST)
 
-    if teacher_subject_obj.group.code != student_group_obj.group.code:
+    if student_obj.group.code not in teacher_subject_list:
         return Response({
             'status': 'error',
             'message': 'Teacher Permission Denied'
