47bdd601e5abe36e215130bc6ff5eae82df44ae4
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2024-06-10 13:12:49 +0500

    Deactivate get questions

--- apps/main/questions/views/retrieve_question.py
+++ apps/main/questions/views/retrieve_question.py
@@ -28,54 +28,54 @@ class QuestionRetrieveAPIView(views.APIView):
             'message': 'GET Question" is temporarily not working'
         }, status=status.HTTP_400_BAD_REQUEST)
 
-    # def post(self, request):
-    #     student_group_letter = get_user_by_token(request).group.code[-1]
-    #
-    #     serializer = self.serializer_class(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #
-    #     subject_code = serializer.validated_data.get('subject_code')
-    #     stage = serializer.validated_data.get('stage')
-    #     number_of_questions = serializer.validated_data.get('number_of_questions')
-    #     question_id = serializer.validated_data.get('question_id')
-    #
-    #     try:
-    #         get_questions = self.model.objects.get(
-    #             subject__code=subject_code,
-    #             stage=stage,
-    #             language=groups_picker[student_group_letter]
-    #         )
-    #     #     ISSUE CATCHER
-    #     except Question.MultipleObjectsReturned as ex:
-    #         return Response({
-    #             'status': 'error',
-    #             'message': f'Check subject. That is not unique. exception: {ex}. query_params: {subject_code, stage, groups_picker[student_group_letter]}'
-    #         }, status=status.HTTP_400_BAD_REQUEST)
-    #     except Question.DoesNotExist:
-    #         return Response({
-    #             'status': 'error',
-    #             'message': 'Check subject or stage and Try Again'
-    #         }, status=status.HTTP_400_BAD_REQUEST)
-    #
-    #     if stage == 1 or stage == 3:
-    #         response = get_question_case_1_3(
-    #             request=request,
-    #             question_obj=get_questions,
-    #             stage=stage,
-    #             question_id=question_id,
-    #             num_questions=number_of_questions
-    #         )
-    #         return response
-    #     elif stage == 2:
-    #         response = get_question_case_2(
-    #             question_obj=get_questions,
-    #             file_path=get_questions.file.name,
-    #             question_id=question_id,
-    #             num_questions=number_of_questions
-    #         )
-    #         return response
-    #     else:
-    #         return Response({
-    #             'status': 'error',
-    #             'message': 'Incorrect stage'
-    #         }, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        student_group_letter = get_user_by_token(request).group.code[-1]
+    
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+    
+        subject_code = serializer.validated_data.get('subject_code')
+        stage = serializer.validated_data.get('stage')
+        number_of_questions = serializer.validated_data.get('number_of_questions')
+        question_id = serializer.validated_data.get('question_id')
+    
+        try:
+            get_questions = self.model.objects.get(
+                subject__code=subject_code,
+                stage=stage,
+                language=groups_picker[student_group_letter]
+            )
+        #     ISSUE CATCHER
+        except Question.MultipleObjectsReturned as ex:
+            return Response({
+                'status': 'error',
+                'message': f'Check subject. That is not unique. exception: {ex}. query_params: {subject_code, stage, groups_picker[student_group_letter]}'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        except Question.DoesNotExist:
+            return Response({
+                'status': 'error',
+                'message': 'Check subject or stage and Try Again'
+            }, status=status.HTTP_400_BAD_REQUEST)
+    
+        if stage == 1 or stage == 3:
+            response = get_question_case_1_3(
+                request=request,
+                question_obj=get_questions,
+                stage=stage,
+                question_id=question_id,
+                num_questions=number_of_questions
+            )
+            return response
+        elif stage == 2:
+            response = get_question_case_2(
+                question_obj=get_questions,
+                file_path=get_questions.file.name,
+                question_id=question_id,
+                num_questions=number_of_questions
+            )
+            return response
+        else:
+            return Response({
+                'status': 'error',
+                'message': 'Incorrect stage'
+            }, status=status.HTTP_400_BAD_REQUEST)
